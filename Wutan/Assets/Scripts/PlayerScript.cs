cb534e4c4469f1dc7b4264b869c483c9f93cf716
dendefo <daniek332@gmail.com>
2023-05-05 02:52:09 +0300

    Push

--- Wutan/Assets/Scripts/PlayerScript.cs
+++ Wutan/Assets/Scripts/PlayerScript.cs
@@ -5,16 +5,39 @@ using UnityEngine.InputSystem;
 
 public class PlayerScript : Character
 {
-
+    private bool isTouchingFloor = true;
     private void Update()
     {
         float x = Input.GetAxis("Horizontal") * Speed * Time.deltaTime;
-        float y = Input.GetAxis("Jump")*JumpForce* Time.deltaTime;
-        Move(new Vector3(x, y, 0));
+        float y = JumpForce;
+        if (!isTouchingFloor) y = 0;
+
+        
+        Move(new Vector3(x, 0, 0));
+        Debug.Log(Input.GetAxis("Jump"));
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Floor" && transform.position.y > collision.transform.position.y)
+        {
+
+            isTouchingFloor = true;
+        }
+
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Floor" && transform.position.y > collision.transform.position.y)
+        {
+
+            isTouchingFloor = false;
+        }
+    }
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+
     }
 }
