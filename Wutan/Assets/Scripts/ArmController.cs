cdcc7795eda5a4e6af29c568e2fb1aa78b9d9c42
dendefo <daniek332@gmail.com>
2023-05-05 17:41:18 +0300

    Arm is Working

--- Wutan/Assets/Scripts/ArmController.cs
+++ Wutan/Assets/Scripts/ArmController.cs
@@ -2,13 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting.Dependencies.Sqlite;
 using UnityEngine;
-using static UnityEditor.PlayerSettings;
 public class ArmController : MonoBehaviour
 {
-    public Transform target;
-    public float angleRange = 90f; // The range of angles the arm can rotate in
-
-    void LateUpdate()
+    public static ArmController Instance;
+    private void Awake()
+    {
+        Instance = this;
+    }
+    public void ChangeRotation()
     {
         // Get the position of the mouse in screen space
         Vector3 mousePosScreen = Input.mousePosition;
@@ -19,14 +20,13 @@ public class ArmController : MonoBehaviour
         // Calculate the angle between the arm and the mouse position
         Vector3 armPos = transform.position;
         Vector3 targetPos = new Vector3(mousePosWorld.x, mousePosWorld.y, armPos.z);
-        float angle = Mathf.Atan2(targetPos.y - armPos.y, targetPos.x - armPos.x) * Mathf.Rad2Deg;
 
         // Check if the angle is within the range of 0-90 or 270-360 degrees
         var norm = Vector3.Normalize(armPos - targetPos);
         var Acos = Mathf.Acos(norm.y);
         var z = Acos / Mathf.PI * 180;
         transform.localEulerAngles = new Vector3(0, 0, z-90);
-        if (Input.mousePosition.x > Screen.width / 2)
+        if (mousePosScreen.x > Screen.width / 2)
         {
             GameManager.Instance.PlayerScriptplayer.IsLookingRight = true;
 
