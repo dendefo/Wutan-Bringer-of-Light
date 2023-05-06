bb9eba7ded953de1b0a2db293d6b62e6877f84a7
Jonathan Blau <122023139+CoolSandwich20@users.noreply.github.com>
2023-05-06 03:11:29 +0300

    PUUUULllll

--- Wutan/Assets/Scripts/MagicMenu.cs
+++ Wutan/Assets/Scripts/MagicMenu.cs
@@ -12,6 +12,15 @@ public class MagicMenu : MonoBehaviour
     public float LastClick;
     public float delay = 0.1f;
     [SerializeField] Transform Staff;
+    [SerializeField] public float CDForFire;
+    [SerializeField] public float CDForThunder;
+    [SerializeField] public float CDForLight;
+    [SerializeField] public float CDForAir;
+    [SerializeField] public float LastUseForFire = 0;
+    [SerializeField] public float LastUseForThunder = 0;
+    [SerializeField] public float LastUseForLight = 0;
+    [SerializeField] public float LastUseForAir = 0;
+
     private void Start()
     {
         menu.enabled = false;
@@ -41,14 +50,36 @@ public class MagicMenu : MonoBehaviour
         {
             if (MagicMenuButtons.chosen != null&& !isShowing)
             {
+                switch (MagicMenuButtons.chosen.ability)
+                {
+                    case Abilities.Fire:
+                        if (Time.time - LastUseForFire <= CDForFire)
+                            return;
+                        LastUseForFire = Time.time;
+                        break;
+                    case Abilities.Air:
+                        if (Time.time - LastUseForAir <= CDForAir)
+                            return;
+                        LastUseForFire = Time.time;
+                        break;
+                    case Abilities.Light:
+                        if (Time.time - LastUseForLight <= CDForLight)
+                            return;
+                        LastUseForLight = Time.time;
+                        break;
+                    case Abilities.Thunder:
+                        if (Time.time - LastUseForThunder <= CDForThunder)
+                            return;
+                        LastUseForThunder = Time.time;
+                        break;
+                }
                 Instantiate(MagicMenuButtons.chosen.prefab, Staff.position+ MagicMenuButtons.chosen.prefab.transform.position, MagicMenuButtons.chosen.prefab.transform.rotation, null);
-
             }
             isShowing = false;
             menu.enabled = isShowing;
-            
+
 
         }
-        
+
     }
 }
